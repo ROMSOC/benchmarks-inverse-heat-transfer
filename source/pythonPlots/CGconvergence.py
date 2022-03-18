eeb4db5b5e499cfc7cefca7aa8607c0cd8846e03
Andrés Prieto <4800161+maprieto@users.noreply.github.com>
2022-03-18 23:56:06 +0100

    Add files via upload

--- source/pythonPlots/CGconvergence.py
+++ source/pythonPlots/CGconvergence.py
@@ -14,7 +14,7 @@ relErr_L2norm = np.loadtxt("../caseDir/ITHACAoutput/CGtest/relError_L2norm_mat.t
 relErr_LinfNorm = np.loadtxt("../caseDir/ITHACAoutput/CGtest/relError_LinfNorm_mat.txt")
 J = np.loadtxt("../caseDir/costFunctionFull_mat.txt")
 
-iterations = np.linspace(1, len(relErr_LinfNorm), len(relErr_LinfNorm))
+iterations = np.linspace(1, np.size(relErr_LinfNorm), np.size(relErr_LinfNorm))
 iterationTicks = np.linspace(1, 100, 11)
 
 f = plt.figure(2,figsize=(12,8))
@@ -22,7 +22,7 @@ f = plt.figure(2,figsize=(12,8))
 plt.semilogy(iterations, relErr_L2norm,'b-', linewidth=2, label = r'$||\epsilon||_{L^2(\Gamma_{s_{in}})}$')
 plt.semilogy(iterations, relErr_LinfNorm,'k--', linewidth=2, label = r'$||\epsilon||_{L^\infty(\Gamma_{s_{in}})}$')
 #plt.ylim(1e-2,1)
-plt.xlim(1,len(iterations))
+plt.xlim(1,len(iterations)+1)
 
 #plt.xticks(iterationTicks)
 
@@ -35,7 +35,7 @@ f = plt.figure(3,figsize=(12,8))
 plt.semilogy(iterations, J,'b-', linewidth=2)
 plt.xlabel("Iteration", fontsize=25)
 plt.ylabel(r"Cost function, $J$", fontsize=25)
-plt.xlim(1,len(iterations))
+plt.xlim(1,len(iterations)+1)
 plt.grid()
 
 
